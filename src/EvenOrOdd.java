dc8d5797d02996e93d24f61c210dd37007296560
Leena Khote <leenakhote23@gmail.com>
2015-03-30 15:28:34 +0530

    First example

---
+++ src/EvenOrOdd.java
@@ -0,0 +1,22 @@
+/**
+ * Created by leena on 30/03/15.
+ */
+public class EvenOrOdd {
+
+    public static int CheckEvenOrOdd(int num) {
+        if (num > 2) {
+            int number = num - 2;
+            num = CheckEvenOrOdd(number);
+        }
+        return num;
+    }
+
+    public static void main(String[] args) {
+        int num = CheckEvenOrOdd(5322221);
+        if (num == 1) {
+            System.out.println("Odd number");
+        } else {
+            System.out.println("Even number");
+        }
+    }
+}
